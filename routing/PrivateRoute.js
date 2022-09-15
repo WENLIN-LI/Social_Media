96c13b04cce656177c1acc491d738f00ce2a2b29
李文琳 <liwenlin@client-10-229-132-109.tamulink.tamu.edu>
2022-09-15 14:00:54 -0500

    start project

---
+++ routing/PrivateRoute.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+
+// const PrivateRoute = ({
+//   component: Component,
+//   auth: { isAuthenticated, loading },
+//   ...rest
+// }) => (
+//   <Route
+//     {...rest}
+//     renderj={(props) =>
+//       !isAuthenticated && !loading ? (
+//         <Navigate to='/login' />
+//       ) : (
+//         <Component {...props} />
+//       )
+//     }
+//   />
+// );
+const PrivateRoute = ({
+  component: Component,
+  auth: { isAuthenticated, loading },
+}) => {
+  if (loading) return <Spinner />;
+  if (isAuthenticated) return <Component />;
+
+  return <Navigate to='/login' />;
+};
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const myStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(myStateToProps)(PrivateRoute);
